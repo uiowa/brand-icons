781c6d2eaccb1476c139cd83f4143125f6e01eb8
Dustin Quam <quamsta@gmail.com>
2022-05-10 10:45:51 -0500

    switched to font awesome close button

--- src/components/IconModal.vue
+++ src/components/IconModal.vue
@@ -8,19 +8,11 @@
         <svg
           @click="closeModal()"
           xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="currentColor"
-          class="bi bi-x-lg"
-          viewBox="0 0 16 16"
+          viewBox="0 0 320 512"
         >
+          <!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
           <path
-            fill-rule="evenodd"
-            d="M13.854 2.146a.5.5 0 0 1 0 .708l-11 11a.5.5 0 0 1-.708-.708l11-11a.5.5 0 0 1 .708 0Z"
-          />
-          <path
-            fill-rule="evenodd"
-            d="M2.146 2.146a.5.5 0 0 0 0 .708l11 11a.5.5 0 0 0 .708-.708l-11-11a.5.5 0 0 0-.708 0Z"
+            d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z"
           />
         </svg>
       </div>
@@ -197,6 +189,7 @@ function closeModal(currentCategory, currentSearchTerm) {
     margin-bottom: 10px;
     margin-top: 0;
     svg {
+      width: 15px;
       cursor: pointer;
     }
     button {
